fe49e7e4b73583c46341bd1804f608c58d7e4286
Graziele L. Miranda <lima.graziele@yahoo.com.br>
2023-06-05 23:10:08 -0300

    atualização 05/06

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,25 @@
-var express = require('express');
+var express = require("express");
 const app = require("../app.js");
 var router = express.Router();
 
 router.get("/home/", (req, res) => {
-    res.render("index");
-})    
+  res.render("index");
+});
 
 router.get("/about/", (req, res) => {
-    res.render("about");
-})
+  res.render("about");
+});
+
+router.get("/aulas/", (req, res) => {
+  res.render("aulas");
+});
+
+router.get("/aula1/", (req, res) => {
+  res.render("aula1");
+});
+
+router.get("/aulateste", (req, res) => {
+  res.render("aulaum");
+});
 
 module.exports = router;
