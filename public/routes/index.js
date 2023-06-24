e6a659fb27e01e81e77f9fd8b1616da03c19577e
sanokauan <110543980+KauanSano@users.noreply.github.com>
2023-06-24 16:14:12 -0300

    Delete public/routes directory

--- public/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-const app = require("../app.js");
-var router = express.Router();
-
-router.get("/home", (req, res) => {
-    res.sendFile(path.join(__dirname+'\index.html'));
-})
-
-module.exports = router
